f142eb63d62d089a67e3634bd4d4b85ae7efc867
elias <eliastoukolehto@gmail.com>
2025-03-13 19:49:25 +0200

    add migrations

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -1,6 +1,6 @@
 import User from "./user";
 
-User.sync()
+
 
 export {
   User
--- backend/src/utils/db.ts
+++ backend/src/utils/db.ts
@@ -1,11 +1,13 @@
 import { Sequelize } from "sequelize"
 import { DATABASE_URL } from "./config"
+import { SequelizeStorage, Umzug } from "umzug"
 
 const sequelize = new Sequelize(DATABASE_URL)
 
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate()
+    await migrator.up()
     console.log('connected to the database')
   } catch {
     console.log('database connection failed')
@@ -14,6 +16,16 @@ const connectToDatabase = async () => {
   return null
 }
 
+const migrationConf = {
+  migrations: { glob: 'src/migrations/*.ts' },
+  context: sequelize.getQueryInterface(),
+  storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
+  logger: console,
+}
+
+export const migrator = new Umzug(migrationConf)
+
+export type Migration = typeof migrator._types.migration
 
 export default {
   connectToDatabase, sequelize
