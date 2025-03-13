9856f7b68cf3234a00c7c322d0690338d81d2c8e
elias <eliastoukolehto@gmail.com>
2025-03-13 19:37:27 +0200

    fix DATABASE_URL

--- backend/src/utils/config.ts
+++ backend/src/utils/config.ts
@@ -10,8 +10,8 @@ export const getEnv = (varName: string): string => {
   
 
 export const DATABASE_URL = process.env.NODE_ENV === 'test' 
-  ? getEnv("DATABASE_URL")
-  : getEnv("TEST_DATABASE_URL")
+  ? getEnv("TEST_DATABASE_URL")
+  : getEnv("DATABASE_URL")
 
 
 
