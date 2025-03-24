9af1a25645d68e21a99b8af12b7a024b180df014
elias <eliastoukolehto@gmail.com>
2025-03-24 19:01:35 +0200

    fix typo

--- backend/src/graphql/resolvers/index.ts
+++ backend/src/graphql/resolvers/index.ts
@@ -4,7 +4,7 @@ import jwt from 'jsonwebtoken'
 import { getEnv } from '../../utils/config'
 import bcrypt from "bcrypt"
 
-//One capital lietter, lowercase letter and number required, at least 8 letters
+//One capital letter, lowercase letter and number required, at least 8 letters
 const passwordRegExp = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,30}$/
 
 const resolvers = {
