1cda90098b45080f442433d2a52bebcd972e0e2a
elias <eliastoukolehto@gmail.com>
2025-04-01 16:14:36 +0300

    fix vite proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/graphql': {
-        target: 'http://localhost:4000/',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  const backend_uri = (env.APP_ENV === "container") 
+    ? "http://backend:4000/graphql"
+    : 'http://localhost:4000/graphql'
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/graphql': {
+          target: backend_uri,
+          changeOrigin: true,
+        },
       },
-    },
-    watch: { usePolling: true }
+      watch: { usePolling: true }
+    }
   }
+
 })
