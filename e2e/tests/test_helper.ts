dd471def24ccc342d25ed944258a07ba0932ec68
elias <eliastoukolehto@gmail.com>
2025-04-05 12:40:14 +0300

    add linting to e2e test files

--- e2e/tests/test_helper.ts
+++ e2e/tests/test_helper.ts
@@ -1,12 +1,11 @@
-import { Page } from "@playwright/test"
-
+import { Page } from '@playwright/test'
 
 const createUser = async (page: Page, username: string, password: string) => {
-  await page.getByRole('link', { name: "Sign In" }).click()
-  await page.getByLabel("Username").fill(username)
-  await page.getByLabel("Password", {exact: true}).fill(password)
-  await page.getByLabel("Confirm password", {exact: true}).fill(password)
-  await page.getByRole('button', { name: "Sign In" }).click()
+  await page.getByRole('link', { name: 'Sign In' }).click()
+  await page.getByLabel('Username').fill(username)
+  await page.getByLabel('Password', { exact: true }).fill(password)
+  await page.getByLabel('Confirm password', { exact: true }).fill(password)
+  await page.getByRole('button', { name: 'Sign In' }).click()
   await page.getByRole('link', { name: /recipe-app/i }).click()
 }
 
--- e2e/tests/user.spec.ts
+++ e2e/tests/user.spec.ts
@@ -2,40 +2,39 @@ import { expect, test } from '@playwright/test'
 import { createUser, backendURL, frontendURL } from './test_helper'
 
 test.describe('Recipe app user', () => {
-
   test.beforeEach(async ({ page, request }) => {
-    const res = await request.post(backendURL, {
+    await request.post(backendURL, {
       data: {
         query: `mutation{
           reset
         }
-        `
-      }
+        `,
+      },
     })
     await page.goto(frontendURL)
   })
 
   test('has frontpage with correct elements', async ({ page }) => {
-    await expect(page.getByRole('link', { name: "Login" })).toBeVisible()
-    await expect(page.getByRole('link', { name: "Sign In" })).toBeVisible()
-    await expect(page.getByRole('link', { name: "Recipe-app" })).toBeVisible()
+    await expect(page.getByRole('link', { name: 'Login' })).toBeVisible()
+    await expect(page.getByRole('link', { name: 'Sign In' })).toBeVisible()
+    await expect(page.getByRole('link', { name: 'Recipe-app' })).toBeVisible()
   })
 
   test('can create an account', async ({ page }) => {
-    await page.getByRole('link', { name: "Sign In" }).click()
-    await page.getByLabel("Username").fill("TestUser")
-    await page.getByLabel("Password", {exact: true}).fill("ValidPassword1")
-    await page.getByLabel("Confirm password", {exact: true}).fill("ValidPassword1")
-    await page.getByRole('button', { name: "Sign In" }).click()
+    await page.getByRole('link', { name: 'Sign In' }).click()
+    await page.getByLabel('Username').fill('TestUser')
+    await page.getByLabel('Password', { exact: true }).fill('ValidPassword1')
+    await page.getByLabel('Confirm password', { exact: true }).fill('ValidPassword1')
+    await page.getByRole('button', { name: 'Sign In' }).click()
     await expect(page.getByText(/Sign In Successful/i)).toBeVisible()
   })
 
   test('can log in to existing account', async ({ page }) => {
-    await createUser(page, "TestUser2", "ValidPassword1")
-    await page.getByRole('link', { name: "Login" }).click()
-    await page.getByLabel("Username").fill("TestUser2")
-    await page.getByLabel("Password").fill("ValidPassword1")
-    await page.getByRole('button', { name: "Login" }).click()
+    await createUser(page, 'TestUser2', 'ValidPassword1')
+    await page.getByRole('link', { name: 'Login' }).click()
+    await page.getByLabel('Username').fill('TestUser2')
+    await page.getByLabel('Password').fill('ValidPassword1')
+    await page.getByRole('button', { name: 'Login' }).click()
     await expect(page.getByText(/Login Successful/i)).toBeVisible()
   })
 })
