6c5b0caa5381172b1d97d1f9cfee6961d37455d5
elias <eliastoukolehto@gmail.com>
2025-06-11 16:53:34 +0300

    add searching recipes in frontend

---
+++ frontend/src/components/RecipeList.tsx
@@ -0,0 +1,41 @@
+import { Button, Grid2 as Grid, Paper, Typography } from '@mui/material'
+import { Link } from 'react-router-dom'
+import { RecipeListItem } from '../types/recipe'
+
+const RecipeList = ({ recipes }: { recipes: RecipeListItem[] }) => {
+  return (
+    <>
+      <Grid container spacing={2}>
+        {recipes.map((recipe, index) => (
+          <Grid size={{ xs: 12, sm: 6, md: 4, lg: 3 }} key={index}>
+            <Paper sx={{ aspectRatio: 1, padding: 2 }}>
+              <Grid container direction="column" spacing={2} sx={{ aspectRatio: 1, justifyContent: 'space-between' }}>
+                <Grid size={12}>
+                  <Typography noWrap variant="h6">{recipe.name}</Typography>
+                </Grid>
+                <Grid size="grow">
+                  <Typography sx={{ overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                    display: '-webkit-box',
+                    WebkitLineClamp: '3',
+                    WebkitBoxOrient: 'vertical',
+                  }}
+                  >
+                    {recipe.description}
+                  </Typography>
+                </Grid>
+                <Grid size="auto">
+                  <Button component={Link} to={`recipes/${recipe.id}`} variant="outlined">
+                    View
+                  </Button>
+                </Grid>
+              </Grid>
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </>
+  )
+}
+
+export default RecipeList
