a0b32fe81bd1b9b5f77e642ca9b365de2a8d8654
elias <eliastoukolehto@gmail.com>
2025-07-04 14:46:40 +0300

    fix crash on invalid token

--- backend/src/middleware/tokenExtractor.ts
+++ backend/src/middleware/tokenExtractor.ts
@@ -3,11 +3,17 @@ import jwt, { JwtPayload } from 'jsonwebtoken'
 import { getEnv } from '../utils/config'
 
 export const tokenExtractor = (req: Request | null): JwtPayload | null => {
-  const auth = req ? req.headers.authorization : null
-  if (auth && auth.startsWith('Bearer ')) {
-    const SECRET = getEnv('SECRET')
-    const decodedToken = jwt.verify(auth.substring(7), SECRET)
-    if (typeof decodedToken !== 'string') return decodedToken
+  try {
+    const auth = req ? req.headers.authorization : null
+    if (auth && auth.startsWith('Bearer ')) {
+      const SECRET = getEnv('SECRET')
+      const decodedToken = jwt.verify(auth.substring(7), SECRET)
+      if (typeof decodedToken !== 'string') return decodedToken
+    }
+    return null
   }
-  return null
+  catch {
+    console.log('Error with token extraction:')
+    return null
+  }
 }
